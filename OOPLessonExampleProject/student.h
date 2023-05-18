de50cc25255f7f6549fcbd1e4734c6ebc26181b6
SlivoviyKompot <slavagming@gmail.com>
2023-05-18 21:17:10 +0300

    Добавьте файлы проекта.

---
+++ OOPLessonExampleProject/student.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "main.h"
+
+class Student {
+private:
+	string name;
+	string surname;
+	int age;
+	float avg_mark;
+
+public:
+	static const int MIN_MARK = 0;
+	static const int MAX_MARK = 10;
+
+	Student();
+	Student(string n, string sname, int a, float mark);
+	Student(string n, string sname);
+	
+	~Student();
+
+	string get_name();
+	void set_name(string n);
+	string get_surname();
+	void set_surname(string sname);
+	int get_age();
+	void set_age(int a);
+	float get_avg_mark();
+	void set_avg_mark(float mark);
+
+	string convert_to_string();
+};
